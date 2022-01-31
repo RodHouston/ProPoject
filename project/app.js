1f58c622b0407a88f7d97e0477ce6896b0187bf2
Roderick Houston <rsjm318@yahoo.com>
2022-01-31 12:06:06 -0500

    finished working on webgame in mobile view

--- project/app.js
+++ project/app.js
@@ -2,6 +2,8 @@ const photos = ['../photos/js1.png', '../photos/js2.png', '../photos/cat.png',
 '../photos/cat2.png', '../photos/css.png', '../photos/html.jpg',
 '../photos/terminalGames.png', '../photos/spaceBattle.png', '../photos/finalPink.png',
 '../photos/font.png', '../photos/ink2.png', '../photos/css2.png' ]
+const description= ['Snippit HTML code using Math.Random function to print random strings from an array', 'Rock, Paper, Scissors Teminal game made with HTML', 'First web page from an homework assignment. Designed using HTML and CSS', 'Connected web page from an homework assignment. Designed using HTML and CSS', 'Code snippits from CSS style sheets', 'Code snippit showcasing some HTML', 'Terminal Game from an hmework assignment: Choose your own adventure', 'Screenshot of a web game made with HTML, CSS, Javascript and jquery','Grapic designed using Adobe Photoshop', 'T-shirt grapic designed using Adobe Photoshop', 'T-shirt grapic designed using Adobe Photoshop', 'CSS styling snippit' ]
+
 
 $(() => {
 
@@ -11,7 +13,7 @@ console.log(photos);
 ///////////working on how to get the pictures to zoom on click
 
 let  zoom = document.getElementById("zoomBox");
-
+let cap = document.getElementById('captionBox')
 
 for (let i = 0; i < photos.length; i++) {
   const $divBox = $('<div>').attr(`id`, `div${i}`)
@@ -21,6 +23,9 @@ for (let i = 0; i < photos.length; i++) {
     zoom.style.backgroundImage = `url(${photos[i]})`
     zoom.style.display = 'block'
     zoom.style.zIndex = "5"
+    // let p = document.createTextNode(description[i]);
+    // p.appendTo('cap')
+    cap.innerHTML = description[i]
 
 
    console.log(photos[i]);
